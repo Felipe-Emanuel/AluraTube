6e394d41adbf811d6d323a75eef0c2d03215ddcc
Felipe <felipesullivan50@gmail.com>
2022-11-07 18:06:56 -0300

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,84 @@
+import config from "../config.json";
+import styled from "styled-components";
+import { CSSReset } from "../src/components/CSSReset";
+import Menu from "../src/components/Menu";
+import { StyledTimeline } from "../src/components/TimeLine";
+
+function HomePage() {
+  const estilosDaHomePage = { 
+    // backgroundColor: "#F00" 
+  };
+
+  return (
+    <>
+      <CSSReset />
+      <div style={estilosDaHomePage}>
+        <Menu />
+        <Header />
+        <TimeLine playlists={config.playlists} />
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
+
+const StylesHeader = styled.div`
+  img {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+  }
+  .user-info {
+    margin-top: 50px;
+    display: flex;
+    align-items: center;
+    width: 100%;
+    padding: 16px 32px;
+    gap: 16px;
+  }
+`;
+
+function Header() {
+  return (
+    <StylesHeader>
+      {/* <img src="banner" /> */}
+
+      <section className="user-info">
+        <img src={`https://github.com/${config.github}.png`} />
+        <div>
+          <h2>{config.name}</h2>
+          <p>{config.job}</p>
+        </div>
+      </section>
+    </StylesHeader>
+  );
+}
+
+function TimeLine(props) {
+  const playlistNames = Object.keys(props.playlists);
+
+  return (
+    <StyledTimeline>
+      {playlistNames.map((playlistName) => {
+        const videos = props.playlists[playlistName];
+        console.log(videos);
+        return (
+          <section>
+            <h2>{playlistName}</h2>
+            <div>
+              {videos.map((video) => {
+                return (
+                  <a href={video.url}>
+                    <img src={video.thumb} />
+                    <span>{video.title}</span>
+                  </a>
+                );
+              })}
+            </div>
+          </section>
+        );
+      })}
+    </StyledTimeline>
+  );
+}
